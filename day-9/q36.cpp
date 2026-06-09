2ae26089b279badf8e487b01c0eb51dfce268a46
raunak sharma <raunakrssharma@gmail.com>
2026-06-09 13:28:25 +0530

    p36 done

---
+++ day-9/q36.cpp
@@ -0,0 +1,19 @@
+//prog to print hollow square 
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5,i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(i==0||i==n-1||j==0||j==n-1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
