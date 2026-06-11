be368c0d8e3f40d01883fc24fa37ff1f27f5aa94
raunak sharma <raunakrssharma@gmail.com>
2026-06-11 15:54:44 +0530

    p44 done

---
+++ day11/q44.cpp
@@ -0,0 +1,21 @@
+//prog to find factorial using func
+#include<iostream>
+using namespace std;
+int fact(int a){
+    int ans=1;
+    if(a==0||a==1){
+        return 1;
+    }
+    for(int i=1;i<=a;i++){
+        ans=ans*i;
+    }
+    return ans;
+} 
+int main(){
+    int n;
+    printf("enter the no : ");
+    scanf("%d",&n);
+    int res=fact(n);
+    cout<<res;
+    return 0;
+}
