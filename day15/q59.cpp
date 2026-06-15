4476ce408eb5b66bec56d6ed047bae6c8eb230aa
raunak sharma <raunakrssharma@gmail.com>
2026-06-15 23:44:14 +0530

    Add files via upload

---
+++ day15/q59.cpp
@@ -0,0 +1,23 @@
+//prog to move array to the right 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int temp=arr[n-1];
+    for(int i=n-1; i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    cout<<"Array after right rotation: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
+    return 0;
+}
