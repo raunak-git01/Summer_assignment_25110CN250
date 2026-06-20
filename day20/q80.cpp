a434f0a2ffad18d09b200863ae3948f7178ebfab
raunak sharma <raunakrssharma@gmail.com>
2026-06-20 14:38:22 +0530

    p80 done

---
+++ day20/q80.cpp
@@ -0,0 +1,23 @@
+//prog to find col wise sum 
+#include <iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter rows and columns: ";
+    cin>>r>>c;
+    int a[r][c];
+    cout<<"Enter matrix elements:\n";
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int j=0;j<c;j++){
+        int sum=0;
+        for(int i=0;i<r;i++){
+            sum=sum+a[i][j];
+        }
+        cout<<"Sum of column "<<j+1<<" = "<<sum<<endl;
+    }
+    return 0;
+}
