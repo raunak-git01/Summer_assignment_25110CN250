0de8473c70539ac808656c748b0046b111e95cc3
raunak sharma <raunakrssharma@gmail.com>
2026-06-20 14:37:41 +0530

    Add files via upload

---
+++ day20/q79.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter rows and columns: ";
+    cin>>r>>c;
+    int a[r][c];
+    cout<<"Enter matrix elements:\n";
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<r;i++){
+        int sum=0;
+        for(int j=0;j<c;j++){
+            sum=sum+a[i][j];
+        }
+        cout<<"Sum of row "<<i+1<<" = "<<sum<<endl;
+    }
+
+    return 0;
+}
