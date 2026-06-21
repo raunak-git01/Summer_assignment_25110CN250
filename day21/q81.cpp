f4ecd294bbeec0b097613e75dae42760b98f5e5e
raunak sharma <raunakrssharma@gmail.com>
2026-06-21 07:20:34 +0530

    Add files via upload

---
+++ day21/q81.cpp
@@ -0,0 +1,14 @@
+//prog to get string length without strlen()
+#include <iostream>
+using namespace std;
+int main(){
+    char str[100];
+    int len=0;
+    cout<<"Enter a string: ";
+    cin.getline(str,100);//to traverse the whole string with spaces too
+    while(str[len]!='\0'){
+        len++;
+    }
+    cout<<"Length of string = "<<len;
+    return 0;
+}
