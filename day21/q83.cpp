9d692367c021918972b6f3a24dfdfc035323fd63
raunak sharma <raunakrssharma@gmail.com>
2026-06-21 07:21:36 +0530

    Add files via upload

---
+++ day21/q83.cpp
@@ -0,0 +1,25 @@
+//prog to count vowels and consonants
+#include <iostream>
+using namespace std;
+int main(){
+    char str[100];
+    int vowels=0,consonants=0;
+    cout<<"Enter a string: ";
+    cin.getline(str,100);
+    for(int i=0;str[i]!='\0';i++){
+        if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z')){
+
+            if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+               str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'){
+                vowels++;
+            }
+            else{
+                consonants++;
+            }
+        }
+    }
+
+    cout<<"Number of vowels = "<<vowels<<endl;
+    cout<<"Number of consonants = "<<consonants<<endl;
+    return 0;
+}
