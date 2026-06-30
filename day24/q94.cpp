1dae15a58282f35d1c0ff398125551b915c91800
raunak sharma <raunakrssharma@gmail.com>
2026-06-30 12:35:41 +0530

    Add files via upload

---
+++ day24/q94.cpp
@@ -0,0 +1,21 @@
+//prog to compress a string
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str,result="";
+    cout<<"Enter the string: ";
+    cin>>str;
+    int count=1;
+    for (int i=0;i<str.length();i++){
+        if (i<str.length()-1&&str[i]==str[i+1]){
+            count++;
+        } else{
+            result+=str[i];
+            result+=to_string(count);
+            count=1;
+        }
+    }
+    cout<<"Compressed string: "<<result;
+    return 0;
+}
